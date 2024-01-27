fdbe9ddcb04066c5f2f356ff3c7bf0f9ee167482
Geraldo Gomes <contato.gneto@gmail.com>
2024-01-27 19:26:35 -0300

    feat: inital structure for search bar

--- src/components/Feed/Navbar.tsx
+++ src/components/Feed/Navbar.tsx
@@ -5,12 +5,19 @@ import Link from 'next/link'
 export default function Navbar() {
    return (
       <nav className="w-full bg-custom-black h-16 text-white flex items-center justify-center">
-         <Link className="container" href={`/`}>
-            <div className="w-full flex items-center gap-3">
+         <div className="container flex items-center">
+            <Link href={`/`} className="w-full flex items-center gap-3">
                <Image src={play} width={44} height={44} alt="PlayIcon" />
-               <h1 className="text-2xl font-bold">EVOFLIX</h1>
+               <h1 className="text-2xl lg:block hidden font-bold">EVOFLIX</h1>
+            </Link>
+            <div className="container absolute text-center flex justify-center">
+               <input
+                  className="w-96 p-2 rounded-md text-black border-none outline-none z-10"
+                  type="text"
+                  placeholder="Filtrar filme"
+               />
             </div>
-         </Link>
+         </div>
       </nav>
    )
 }
